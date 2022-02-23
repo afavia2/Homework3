3ee69f4dadbc1bbfcef559950a706135dbd2acf9
afavia2 <91914070+afavia2@users.noreply.github.com>
2022-02-23 14:54:28 -0600

    Add files via upload

---
+++ hw/FlightManager.java
@@ -0,0 +1,40 @@
+package hw;
+
+import hw.airline.Airline;
+import hw.airport.Airport;
+import hw.flight.Flight;
+import hw.flight.FlightFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public final class FlightManager {
+
+    private static FlightManager ourInstance;
+    private static List<Flight> flights;
+
+    public static FlightManager getInstance() throws Exception {
+        if (ourInstance == null)
+            ourInstance = new FlightManager();
+
+        return ourInstance;
+    }
+
+    private FlightManager() {
+        flights = new ArrayList<Flight>();
+    }
+
+    public String createFlight(String type, Airline airline, Airport origin, Airport destination) {
+        Flight flight = FlightFactory.createFlight(type, airline, origin, destination);
+        flights.add(flight);
+
+        return flight.getFlightNumber();
+    }
+
+    public Optional<Flight> getFlightByFlightNumber(String flightNumber) {
+        return flights.stream()
+                .filter(flt -> flt.getFlightNumber().equals(flightNumber))
+                .findFirst();
+    }
+}
